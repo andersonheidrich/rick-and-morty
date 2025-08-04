bb9c4b934dcb7de0a203866c34a36a9ca6080310
andersonheidrich <andersonheidrich@hotmail.com>
2025-08-04 17:33:22 -0300

    feat: created pagination

--- src/graphql/queries.tsx
+++ src/graphql/queries.tsx
@@ -3,6 +3,10 @@ import { gql } from "@apollo/client";
 export const GET_CHARACTERS = gql`
   query GetCharacters($page: Int = 1, $name: String) {
     characters(page: $page, filter: { name: $name }) {
+      info {
+        next
+        prev
+      }
       results {
         id
         name
