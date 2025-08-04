c1129c408da9e86164eb69890fc434d98e6ca0d0
andersonheidrich <andersonheidrich@hotmail.com>
2025-08-04 08:27:20 -0300

    style: scroll bar removed and loading screens adjusted

--- src/components/CharacterCarousel/CharacterCarousel.tsx
+++ src/components/CharacterCarousel/CharacterCarousel.tsx
@@ -6,6 +6,8 @@ import { GET_CHARACTERS } from "@/graphql/queries";
 import Image from "next/image";
 import { CharacterCarouselProps } from "./interfaces";
 import { useFavorites } from "@/hooks/useFavorites";
+import { useRef } from "react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 export default function CharacterCarousel({
   onSelectId,
@@ -14,7 +16,26 @@ export default function CharacterCarousel({
   const { loading, error, data } = useQuery(GET_CHARACTERS);
   const { favorites } = useFavorites();
 
-  if (loading) return <p className="text-center">Carregando...</p>;
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  const scrollLeft = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollBy({ left: -300, behavior: "smooth" });
+    }
+  };
+
+  const scrollRight = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollBy({ left: 300, behavior: "smooth" });
+    }
+  };
+
+  if (loading)
+    return (
+      <p className="flex w-full h-[413px] items-center justify-center">
+        Carregando...
+      </p>
+    );
   if (error) return <p className="text-center">Erro: {error.message}</p>;
 
   const characters = onlyFavorites
@@ -28,8 +49,16 @@ export default function CharacterCarousel({
   }
 
   return (
-    <div className="flex w-full h-[413px]">
-      <div className="flex w-full h-[413px] gap-[12px] p-4 overflow-x-auto overflow-y-hidden">
+    <div className="flex w-full h-[413px] items-center">
+      <ChevronLeft
+        className="w-[40px] h-[40px] cursor-pointer hover:scale-130 transition-transform duration-250"
+        onClick={scrollLeft}
+        aria-label="Scroll Left"
+      />
+      <div
+        ref={scrollRef}
+        className="flex w-full h-[413px] gap-[12px] p-4 overflow-x-auto overflow-y-hidden scrollbar-hide"
+      >
         {characters.map((character: any) => (
           <button
             key={character.id}
@@ -49,6 +78,11 @@ export default function CharacterCarousel({
           </button>
         ))}
       </div>
+      <ChevronRight
+        className="w-[40px] h-[40px] cursor-pointer hover:scale-130 transition-transform duration-250"
+        onClick={scrollRight}
+        aria-label="Scroll Right"
+      />
     </div>
   );
 }
